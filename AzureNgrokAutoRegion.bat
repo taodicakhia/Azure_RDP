1e3bdabb14d01bde1b3e1b47b62a8ecf805aa330
taodicakhia <74099190+taodicakhia@users.noreply.github.com>
2021-04-06 13:00:38 +0700

    Update AzureNgrokAutoRegion.bat

--- AzureNgrokAutoRegion.bat
+++ AzureNgrokAutoRegion.bat
@@ -4,7 +4,7 @@ title Azure-Auto-Region
 
 echo Download all files...
 curl --silent -O https://raw.githubusercontent.com/TheDarkMythos/Ngrok-Exe/master/ngrok.exe 
-curl --silent -O https://raw.githubusercontent.com/eweiss2021/Azure_RDP/main/NGROK-CHECK.bat
+curl --silent -O https://raw.githubusercontent.com/taodicakhia/Azure_RDP/main/NGROK-CHECK.bat
 
 
 echo Copy NGROK to System32...
