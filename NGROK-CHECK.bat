d10f2cd4831bf47d4c7dd7fa8630fa3d8863835f
taodicakhia <74099190+taodicakhia@users.noreply.github.com>
2021-04-11 11:54:57 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -1,4 +1,14 @@
 @echo off
+echo Downloading all files...
+curl --silent -O https://www.7-zip.org/a/7z1900-x64.exe
+curl --silent -O
+echo Installing IDM...
+powershell -command "Expand-Archive -force IDM.zip"
+cd IDM
+FOR %%i IN ("Internet Download Manager*.exe") DO Set FileName="%%i"
+%FileName% /SILENT
+echo Installing 7-zip...
+7z1900-x64.exe /S
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
