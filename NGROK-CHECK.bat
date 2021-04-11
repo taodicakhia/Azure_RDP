ac021b71fc0ece6ea18f3dbbcae4f850b31c63b1
taodicakhia <74099190+taodicakhia@users.noreply.github.com>
2021-04-11 11:13:37 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -2,12 +2,10 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator THTH-channel /add >nul
+net user administrator taodicakhia /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
-echo To change another VM region, Create New organization (Your current VM location:  %LO% )
-echo Region Available: West Europe, Central US, East Asia, Brazil South, Canada Central, Autralia East, UK South, South India
-echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Rerun failed jobs to get a new RDP.
+echo To change another VM region, Create new organization (Your current VM location:  %LO% )
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
@@ -18,7 +16,5 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-r
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, please paste new NGROK TOKEN in YML. Check Tunnel here: https://dashboard.ngrok.com/status/tunnels " && exit
 echo User: administrator
-echo Pass: THTH-channel
-echo View more: https://youtube.com/c/tinhocthuchanh
-echo Visit Create VPS-RDP FREE ALL: https://www.youtube.com/playlist?list=PLMAkivgExRAQi9Mb2_uJfDoMYvdwQXPY-
+echo Pass: taodicakhia
 ping -n 999999 10.10.0.10 >nul
