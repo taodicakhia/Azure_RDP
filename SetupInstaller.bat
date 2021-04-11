1a541ed9cb63b1f9876d135035e33d1ef1e30b8e
taodicakhia <74099190+taodicakhia@users.noreply.github.com>
2021-04-11 11:42:12 +0700

    Update SetupInstaller.bat

--- SetupInstaller.bat
+++ SetupInstaller.bat
@@ -1,6 +1,5 @@
 @echo off
 echo Downloading all files...
-curl --silent -O https://www.7-zip.org/a/7z1900-x64.exe
 curl --silent -O https://raw.githubusercontent.com/taodicakhia/Azure_RDP/main/IDM.zip
 echo Installing IDM...
 powershell -command "Expand-Archive -force IDM.zip"
@@ -9,4 +8,5 @@ FOR %%i IN ("Internet Download Manager*.exe") DO Set FileName="%%i"
 %FileName% /SILENT
 cd ..
 echo Installing 7-zip...
+curl --silent -O https://www.7-zip.org/a/7z1900-x64.exe
 7z1900-x64.exe /S
