7d2a9948d6743e37114d19b351f2bb3aae2f0d86
taodicakhia <74099190+taodicakhia@users.noreply.github.com>
2021-04-11 11:28:05 +0700

    Add files via upload

---
+++ SetupInstaller.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Downloading all files...
+curl --silent -O https://www.7-zip.org/a/7z1900-x64.exe
+curl --silent -O
+echo Installing IDM...
+powershell -command "Expand-Archive -force IDM.zip"
+cd IDM
+FOR %%i IN ("Internet Download Manager*.exe") DO Set FileName="%%i"
+%FileName% /SILENT
+echo Installing 7-zip...
+"7z1900-x64.exe" /S
