09b120f5bdfc467713a8b67b64f4df55d22f5fa1
taodicakhia <74099190+taodicakhia@users.noreply.github.com>
2021-10-23 13:22:29 +0700

    Update AzureNgrokAutoRegion.bat

--- AzureNgrokAutoRegion.bat
+++ AzureNgrokAutoRegion.bat
@@ -11,14 +11,5 @@ type info.txt | jq -r .country > country.txt
 set /p IP=<ip.txt
 set /P RE=<country_iso.txt
 set /P LO=<country.txt
-if %RE%==US (start ngrok tcp 3389)
-if %RE%==CA (start ngrok tcp 3389)
-if %RE%==HK (start ngrok tcp --region ap 3389)
-if %RE%==SG (start ngrok tcp --region ap 3389)
-if %RE%==NL (start ngrok tcp --region eu 3389)
-if %RE%==IE (start ngrok tcp --region eu 3389)
-if %RE%==GB (start ngrok tcp --region eu 3389)
-if %RE%==BR (start ngrok tcp --region sa 3389)
-if %RE%==AU (start ngrok tcp --region au 3389)
-if %RE%==IN (start ngrok tcp --region in 3389)
+start ngrok tcp --region ap 3389
 NGROK-CHECK.bat
